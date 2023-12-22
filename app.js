0323c8673455836efc9471450ec82fd3c87c3783
Patrick Swafford <patrick.swafford@swaffordhouse.com>
2023-12-22 01:53:00 -0500

    implement mergeObjects

--- app.js
+++ app.js
@@ -15,3 +15,7 @@ const filterOutOdds = (...nums) => nums.filter(num => num % 2 === 0);
 
 /* Write a function called findMin that accepts a variable number of arguments and returns the smallest argument: */
 const findMin = (...nums) => nums.reduce((min, num) => min <= num ? min : num);
+
+
+/* Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object. */
+const mergeObjects = (o1, o2) => ({...o1, ...o2});
